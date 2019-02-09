d37462add0de79df8add276a959b41b1d0722d08
ohyoon-kwon <47256983+ohyoon-kwon@users.noreply.github.com>
2019-02-08 21:20:56 -0600

    Update simple-queries.sql

--- simple-queries.sql
+++ simple-queries.sql
@@ -1,22 +1,17 @@
+--From our 2010 table, this query is selecting all the data of suspects whom physical force (specifically handcuffs) were
+--used on by the police. The data is in an ascending order of serial numbers.
 Select * From NYC_Stop_and_Frisk.NYC_DATA_2010
 Where pf_hcuff = True
 Order by ser_num
 
+--From our 2011 table, this query leads to a display of all the suspects who were found carrying a pistol. The data is
+--in arranged in order from the earliest time of stop to the latest.
+Select * From NYC_Stop_and_Frisk.NYC_DATA_2011
+Where pistol = True
+Order by timestop
 
-
-Include SQL queries that explores the BigQuery data, and explain what the queries do.
-Each query should be preceded by a comment that explains what it does. All queries
-should be written and saved in ./simple-queries.sql in your group's repository.
-
--20 no queries use the WHERE clause
--20 no queries use the ORDER BY clause
--20/15 each un-queried dataset, depending on amount of datasets
--10 each missing comment
--5 each incorrect comment, or comment too similar to query
-
-
-Add a short comment above each SQL statement to describe the query.
-Comments should begin with a ”--” (e.g. --this is a legal comment
-in SQL).
-● You should have at least 1 query per table. The queries should have a WHERE
-clause and ORDER BY clause.
+--From our 2012 table, this query displays the type of ID, whether or not the arrest was made, gender, and race of
+--all the suspects who were suspected to be involved in robbery. The data is in an ascending order of serial numbers.
+Select N.typeofid, N.arstmade, N.sex, N.race From NYC_Stop_and_Frisk.NYC_DATA_2012 N
+Where crimsusp = "ROBBERY"
+Order by ser_num
