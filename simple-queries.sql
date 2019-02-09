b42d72e924fd195a861f7c3dcf255ee591dcfd6b
ohyoon-kwon <47256983+ohyoon-kwon@users.noreply.github.com>
2019-02-08 18:48:02 -0600

    Create simple-queries.sql

---
+++ simple-queries.sql
@@ -0,0 +1,22 @@
+Select * From NYC_Stop_and_Frisk.NYC_DATA_2010
+Where pf_hcuff = True
+Order by ser_num
+
+
+
+Include SQL queries that explores the BigQuery data, and explain what the queries do.
+Each query should be preceded by a comment that explains what it does. All queries
+should be written and saved in ./simple-queries.sql in your group's repository.
+
+-20 no queries use the WHERE clause
+-20 no queries use the ORDER BY clause
+-20/15 each un-queried dataset, depending on amount of datasets
+-10 each missing comment
+-5 each incorrect comment, or comment too similar to query
+
+
+Add a short comment above each SQL statement to describe the query.
+Comments should begin with a ”--” (e.g. --this is a legal comment
+in SQL).
+● You should have at least 1 query per table. The queries should have a WHERE
+clause and ORDER BY clause.
