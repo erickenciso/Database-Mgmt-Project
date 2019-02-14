afc1a71ce63d3a36768d569e26dab030f5842a2b
ohyoon-kwon <47256983+ohyoon-kwon@users.noreply.github.com>
2019-02-14 00:17:43 -0600

    Update simple-queries.sql

--- simple-queries.sql
+++ simple-queries.sql
@@ -5,14 +5,14 @@ Where city = 'Las Vegas'
 Order by review_count
 
 --From our 'Yelp_Review' table, this query selects all the data of reviews, which more than 50 users believed
---to be useful. The data is in ordered by review IDs.
+--to be useful. The data is ordered by review IDs.
 Select * From Yelp_Dataset.Yelp_Review
 Where useful > 50
 Order by review_id
 
---From our 'Yelp_User' table, this query displays the name of user, review counts, user's yelp account registration
---date, and number of fans of users who have more than 100 fans. The data is ordered by earlist account registration
---date to the latest date.
+--From our 'Yelp_User' table, this query displays the names of users, review counts, user's yelp account registration
+--dates, and number of fans of users who have more than 100 fans. The data is ordered from the earliest account
+--registration date to the latest.
 Select name, review_count, yelping_since, fans From Yelp_Dataset.Yelp_User
 Where fans > 100
 Order by yelping_since
