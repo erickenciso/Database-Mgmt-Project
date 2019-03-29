6d5adc3a8ae98913633e5db04a97de794e29b310
ohyoon-kwon <47256983+ohyoon-kwon@users.noreply.github.com>
2019-03-28 19:19:30 -0500

    Update nested-queries.sql

--- nested-queries.sql
+++ nested-queries.sql
@@ -15,3 +15,21 @@ from Yelp_Dataset.Yelp_User
 where review_count > 1000 and useful > 500 and average_stars >
 (select avg(average_stars)
 from Yelp_Dataset.Yelp_User)
+
+--This query displays the information of businesses which received more than 100 'useful' votes on reviews
+--and have reviews that received star ratings greater than average.
+select yb.business_id, yb.name, yb.city, yb.state, yb.stars, yb.review_count
+from Yelp_Dataset.Yelp_Business yb
+inner join Yelp_Dataset.Yelp_Review yr on yb.business_id = yr.business_id
+where yr.useful > 100 and yr.stars > (select avg(stars) from Yelp_Dataset.Yelp_Review)
+
+--This query displays the number of businesses in each star rating (1-5). This query specifically shows businesses
+--that received reviews from users who have written more than 1000 reviews and have more than 1000 fans.
+select stars, count(business_id) as business_count
+from Yelp_Dataset.Yelp_Review
+where user_id in 
+(select yr.user_id from Yelp_Dataset.Yelp_Review yr
+inner join Yelp_Dataset.Yelp_User yu on yr.user_id = yu.user_id
+where yu.review_count > 1000 and yu.fans > 1000)
+group by stars
+order by stars
