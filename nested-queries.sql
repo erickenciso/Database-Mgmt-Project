80af2f7b525a429703dba007157a2d930541e064
ohyoon-kwon <47256983+ohyoon-kwon@users.noreply.github.com>
2019-03-28 19:34:29 -0500

    Update nested-queries.sql

--- nested-queries.sql
+++ nested-queries.sql
@@ -1,10 +1,10 @@
 --This query displays the total count of restaurants by state that are open, have more than 300 reviews, and
 -- have star ratings of above average
 select state, count(state) as restaurant_count
-from Yelp_Dataset.Yelp_Business
+from `inner-chassis-230421.Yelp_Dataset.Yelp_Business`
 where is_open = 1 and review_count > 300 and stars >
 (select avg(stars)
-from Yelp_Dataset.Yelp_Business)
+from `inner-chassis-230421.Yelp_Dataset.Yelp_Business`)
 group by state
 order by state
 
@@ -26,10 +26,10 @@ where yr.useful > 100 and yr.stars > (select avg(stars) from Yelp_Dataset.Yelp_R
 --This query displays the number of businesses in each star rating (1-5). This query specifically shows businesses
 --that received reviews from users who have written more than 1000 reviews and have more than 1000 fans.
 select stars, count(business_id) as business_count
-from Yelp_Dataset.Yelp_Review
+from `inner-chassis-230421.Yelp_Dataset.Yelp_Review`
 where user_id in 
-(select yr.user_id from Yelp_Dataset.Yelp_Review yr
-inner join Yelp_Dataset.Yelp_User yu on yr.user_id = yu.user_id
+(select yr.user_id from `inner-chassis-230421.Yelp_Dataset.Yelp_Review` yr
+inner join `inner-chassis-230421.Yelp_Dataset.Yelp_User` yu on yr.user_id = yu.user_id
 where yu.review_count > 1000 and yu.fans > 1000)
 group by stars
 order by stars
