efa395e8c5de70eb1384d2c066391d64c4fd724f
erickenciso <35780752+erickenciso@users.noreply.github.com>
2019-03-29 19:26:11 -0500

    Update nested-queries.sql

--- nested-queries.sql
+++ nested-queries.sql
@@ -33,3 +33,59 @@ inner join `inner-chassis-230421.Yelp_Dataset.Yelp_User` yu on yr.user_id = yu.u
 where yu.review_count > 1000 and yu.fans > 1000)
 group by stars
 order by stars
+
+--This query will show the 10 businesses with the same name that have the most above average reviews
+select b.name, count(b.business_id) as number_of_reviews_above_average
+from Yelp_Dataset.Yelp_Review r join Yelp_Dataset.Yelp_Business b on r.business_id = b.business_id 
+where r.stars >= (select avg(stars) from Yelp_Dataset.Yelp_Review)  
+group by b.name
+order by number_of_reviews_above_average desc\
+LIMIT 10
+
+--This query will show the 10 businesses with the same name that have the most below average reviews
+select b.name, count(b.business_id) as number_of_reviews_below_average
+from Yelp_Dataset.Yelp_Review r join Yelp_Dataset.Yelp_Business b on r.business_id = b.business_id 
+where r.stars <= (select avg(stars) from Yelp_Dataset.Yelp_Review)  
+group by b.name
+order by number_of_reviews_below_average desc
+LIMIT 10
+
+--This query will show the number of businesses per state that have a review count that is greater than the average
+select state, count(state) as number_of_businesses
+from Yelp_Dataset.Yelp_Business b
+where b.business_id in (select c.business_id from Yelp_Dataset.Yelp_Business c where c.review_count >= (select avg(review_count) from Yelp_Dataset.Yelp_Business))
+group by state 
+order by number_of_businesses desc
+LIMIT 10
+                                                                                                        
+--This query will show the number of businesses per state that have a review count that is lower than the average
+select state, count(state) as number_of_businesses
+from Yelp_Dataset.Yelp_Business b
+where b.business_id in (select c.business_id from Yelp_Dataset.Yelp_Business c where c.review_count <= (select avg(review_count) from Yelp_Dataset.Yelp_Business))
+group by state 
+order by number_of_businesses desc
+LIMIT 10                                                                                                 
+                                                                                                        
+--This query will show the number of businesses per state that have a review count that is greater than the average
+select state, count(state) as number_of_businesses
+from Yelp_Dataset.Yelp_Business b
+where b.business_id in (select c.business_id from Yelp_Dataset.Yelp_Business c where c.review_count < (select avg(review_count) from Yelp_Dataset.Yelp_Business))
+group by state 
+order by number_of_businesses desc
+LIMIT 10
+                                                                                                       
+--This query will show the difference in avg rating between the stores which have greater than average reviews and whether
+--they are open or closed
+select is_open, avg(r.stars) as avg_rating
+from Yelp_Dataset.Yelp_Business b join Yelp_Dataset.Yelp_Review r on b.business_id = r.business_id
+where b.business_id in (select c.business_id from Yelp_Dataset.Yelp_Business c where c.review_count < (select avg(review_count) from Yelp_Dataset.Yelp_Business))
+group by b.is_open
+                                                                                                       
+                                                                                                       
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
